4e3814fa47c6fb74f58599b695a7c23a4b292a35
jajangscript <jajangkadi007@gmail.com>
2024-12-01 06:28:22 +0800

    NavBar
    Signed-off-by: jajangscript <jajangkadi007@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewsArticles from "./pages/NewsArticles";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/news" element={<NewsArticles />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
