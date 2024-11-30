6f6c58e83ca210b5d59931f18c7e28837f9fc764
jajangscript <jajangkadi007@gmail.com>
2024-12-01 07:06:56 +0800

    Initialisasi
    Signed-off-by: jajangscript <jajangkadi007@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://jajangkdi007:jajangkdi007@cluster0.sdsh3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on  port 3000!");
 });
