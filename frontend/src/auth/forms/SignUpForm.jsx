9c52ad6e1831b5588c7a54ceeff0487900fd2d99
jajangscript <jajangkadi007@gmail.com>
2024-12-07 23:08:55 +0800

    Complete sign up functionality
    Signed-off-by: jajangscript <jajangkadi007@gmail.com>

--- frontend/src/auth/forms/SignUpForm.jsx
+++ frontend/src/auth/forms/SignUpForm.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -15,18 +14,25 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
+// import GoogleAuth from "@/components/shared/GoogleAuth"
 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  email: z.string().min({ message: "Invalid email address." }),
+  email: z.string().email({ message: "Invalid email address." }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters." }),
 });
 
 const SignUpForm = () => {
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   // 1. Define your form.
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -38,10 +44,37 @@ const SignUpForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values) {
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        toast({ title: "Sign up failed! Please try again." });
+
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+
+      if (res.ok) {
+        toast({ title: "Sign up Successful!" });
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+      toast({ title: "Something went wrong!" });
+    }
   }
 
   return (
@@ -67,16 +100,18 @@ const SignUpForm = () => {
         {/* right */}
         <div className="flex-1">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
               <FormField
                 control={form.control}
                 name="username"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Username</FormLabel>
+
                     <FormControl>
                       <Input type="text" placeholder="Username" {...field} />
                     </FormControl>
+
                     <FormMessage />
                   </FormItem>
                 )}
@@ -88,6 +123,7 @@ const SignUpForm = () => {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Email</FormLabel>
+
                     <FormControl>
                       <Input
                         type="text"
@@ -95,6 +131,7 @@ const SignUpForm = () => {
                         {...field}
                       />
                     </FormControl>
+
                     <FormMessage />
                   </FormItem>
                 )}
@@ -109,7 +146,7 @@ const SignUpForm = () => {
                     <FormControl>
                       <Input
                         type="password"
-                        placeholder="**********"
+                        placeholder="Password"
                         {...field}
                       />
                     </FormControl>
@@ -118,17 +155,28 @@ const SignUpForm = () => {
                 )}
               />
 
-              <Button type="submit" className="bg-blue-500 w-full">
-                Sign Up
+              <Button
+                type="submit"
+                className="bg-blue-500 w-full"
+                disabled={loading}
+              >
+                {loading ? (
+                  <span className="animate-pulse">Loading...</span>
+                ) : (
+                  <span>Sign Up</span>
+                )}
               </Button>
+              {/* <GoogleAuth /> */}
             </form>
           </Form>
-          <div>
-            <span>Have an account? </span>
-            <Link to={"/sign-in"} className="text-blue-500">
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {errorMessage && <p className="mt-5 text-red-500">{errorMessage}</p>}
         </div>
       </div>
     </div>
